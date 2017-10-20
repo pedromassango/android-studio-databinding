ec2e9d75abc22bf3e310a064394ed921a18cdceb
PEDRO33 <peter3fashion@gmail.com>
2017-10-20 15:21:34 -0700

    Alterando o nome das classes geradas

--- app/src/main/java/com/pedromassango/databinding/MainActivity.java
+++ app/src/main/java/com/pedromassango/databinding/MainActivity.java
@@ -3,9 +3,8 @@ package com.pedromassango.databinding;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
 
-import com.pedromassango.databinding.databinding.ActivityMainBinding;
+import com.pedromassango.databinding.databinding.BindingMain;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,8 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+       // ActivityMainBinding mainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main);
 
-        ActivityMainBinding mainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        BindingMain mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        //BindingMain mb = BindingMain.inflate(getLayoutInflater());
 
         Usuario usuario = new Usuario();
         usuario.setPrimeiroNome("Pedro");
